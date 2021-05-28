af0f95db7741bd81611f565e702021d0a9b33181
asiful086 <asifulhaque086@gmail.com>
2021-05-28 09:50:17 +0600

    final project

--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,43 @@
-import Head from "next/head"
-import Header from "../components/Header"
-const Search = () => {
-    return (
-        <div>
-            <Head>
-                <title>Search Results</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Header/>
-            {/* Search Result */}
-        </div>
-    )
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Header from "../components/Header";
+import SearchResults from "../components/SearchResults";
+import Response from "../Response";
+const Search = ({ results }) => {
+  const router = useRouter();
+  // console.log(results);
+  return (
+    <div>
+      <Head>
+        <title>{router.query.term} - Google Search</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      {/* Search Result */}
+      <SearchResults results={results} />
+    </div>
+  );
+};
+
+export default Search;
+
+export async function getServerSideProps(context) {
+  const API_KEY = process.env.API_KEY;
+  const CONTEXT_KEY = process.env.CONTEXT_KEY;
+
+  const useDummyData = false;
+  const startIndex = context.query.start || "0";
+
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
+  // Pass the results to the client
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
 
-export default Search
